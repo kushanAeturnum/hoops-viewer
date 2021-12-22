a7cb9c04b0c762967b81bbf744cd51548f8e6981
Gowthamy Anantharasa <gowthamy.a@aeturnum.com>
2021-12-22 19:39:57 +0530

    camera view change of CAD file

---
+++ src/js/CustomOperator.js
@@ -0,0 +1,34 @@
+export class CustomOperator {
+    constructor(viewer) {
+        this._viewer = viewer;
+        this._partId = null;
+    }
+    onMouseDown(event) {
+        const pickConfig = new window.Communicator.PickConfig(window.Communicator.SelectionMask.All);
+        this._viewer.view.pickFromPoint(event.getPosition(), pickConfig).then((selection) => {
+            if (selection.getSelectionType() !== window.Communicator.SelectionType.None) {
+                this._partId = selection.getNodeId();
+                this._viewer.model.setNodesOpacity([this._partId], 0.5);
+                console.log("_partId", this._partId )
+                console.log("selection", selection )
+            }
+        });
+    }
+    onMouseUp(event) {
+        if (this._partId !== null) {
+            this._viewer.model.setNodesOpacity([this._partId], 1.0);
+            this._partId = null;
+        }
+    }
+    onMouseMove(event) { }
+    onMousewheel(event) { }
+    onTouchStart(event) { }
+    onTouchMove(event) { }
+    onTouchEnd(event) { }
+    onKeyDown(event) { }
+    onKeyUp(event) { }
+    onDeactivate() { }
+    onActivate() { }
+    onViewOrientationChange() { }
+    stopInteraction() { }
+}
